31e21e97f0706f8fae409fa6b862850587b00621
Akhil351 <akhil.vathaluru@gmail.com>
2024-10-27 00:21:28 +0530

    simpleValues

---
+++ GoTwo/simpleValues/main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+	// int
+	fmt.Println(1+1)
+	// string
+	fmt.Println("hello Go lang")
+	// bool
+	fmt.Println(true)
+	//float32 or float64
+	fmt.Println(10.5)
+	fmt.Println(7.0/3.0)
+}
