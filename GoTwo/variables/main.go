554b5a297733f144abababf6018c59e4555c8128
Akhil351 <akhil.vathaluru@gmail.com>
2024-10-27 00:28:17 +0530

    variables

---
+++ GoTwo/variables/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string = "golang"
+	var name="golang"
+	var isAdult=true
+	var age int=30
+	name2:="akhil"
+	//var price float32
+	price:=50.5
+	fmt.Println(name)
+	fmt.Println(isAdult)
+	fmt.Println(age)
+	fmt.Println(name2)
+	fmt.Println(price)
+}
