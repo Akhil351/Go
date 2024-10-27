5eb8e232669cd72119e0a9a5fe9c30bb13450b0f
Akhil351 <akhil.vathaluru@gmail.com>
2024-10-27 11:51:03 +0530

    pointers

--- GoTwo/pointers/main.go
+++ GoTwo/pointers/main.go
@@ -1,12 +1,17 @@
 package main
 
 import "fmt"
-func change(num *int){
-	*num=5
-	fmt.Println("in change function",*num)
+func change(a *int){
+	*a=5
+	fmt.Println("in change function",*a)
 }
 func main() {
 	num := 1
 	change(&num)
+	var a,b int=2,5
+	ptr1:=&a
+	ptr2:=&b
+	sum:=*ptr1+*ptr2
 	fmt.Println("in main function",num)
+	fmt.Println(sum)
 }
