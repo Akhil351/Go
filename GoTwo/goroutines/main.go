5b8d880a6e4e481c266faf314466cca26068fe9c
Akhil351 <akhil.vathaluru@gmail.com>
2024-10-27 23:01:03 +0530

    routines

--- GoTwo/goroutines/main.go
+++ GoTwo/goroutines/main.go
@@ -1,4 +1,24 @@
 package main
-func main(){
-	
+
+import (
+	"fmt"
+	"time"
+)
+
+func sayHello() {
+	fmt.Println("Hello")
+	time.Sleep(time.Second * 2)
+	fmt.Println("hello function completed")
+}
+func sayHi() {
+	fmt.Println("Hi")
+	time.Sleep(time.Second)
+	fmt.Println("hi function completed")
+}
+
+func main() {
+	fmt.Println("main function ")
+	go sayHello()
+	go sayHi()
+	time.Sleep(time.Second * 3)
 }
